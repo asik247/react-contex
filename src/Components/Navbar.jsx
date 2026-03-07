6d672a49e9c15d5cdd311afacce5b9d14e92ab5f
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-07 22:35:26 +0600

    creat contex and provider and value pass

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Footer from './Footer';
+
+const Navbar = () => {
+    return (
+        <div>
+            <h1>Iam Navbar code</h1>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Navbar;
